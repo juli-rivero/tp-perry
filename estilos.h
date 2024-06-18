97a69df3192c1efad1efaf9e6488f0dcb9524774
Juli <jarivero@fi.uba.ar>
2024-06-18 18:32:00 -0300

    pre y post en estilos.h

--- estilos.h
+++ estilos.h
@@ -49,26 +49,38 @@ styled_char_t* crear_styled_char();
 // Libera la memoria de un styled_char_t previamente creado por crear_styled_char(). En caso de ser un NULL no pasa nada.
 void destruir_styled_char(styled_char_t* s_char);
 
+// Guarda el caracter en s_char
 void set_caracter(styled_char_t* s_char, char caracter);
 
+// Devuelve el caracter de s_char
 char get_caracter(styled_char_t* s_char);
 
+// Guarda el atributo del texto en s_char
 void set_estilo(styled_char_t* s_char, texto_atributo_t estilo);
 
+// Guarda el color del texto en s_char
 void set_color_texto(styled_char_t* s_char, color_t color);
 
+// Guarda el color del fondo en s_char
 void set_color_fondo(styled_char_t* s_char, color_t color);
 
+// Guarda el modo del fondo en s_char
 void set_modo_texto(styled_char_t* s_char, modo_texto_t modo);
 
+// Guarda el modo del fondo en s_char
 void set_modo_fondo(styled_char_t* s_char, modo_fondo_t modo);
 
+// Guarda si se desea resetear el formato luego de llamar a imprimir_styled_char
 void set_reset(styled_char_t* s_char, bool reset);
 
+// Imprime el s_char
+// Se va imprimir el caracter con todos los atributos/colores que se hayan guardado/sobrescrito en éste.
 void imprimir_styled_char(styled_char_t* s_char);
 
+// Setea el estilo para cualquier texto siguiente que se imprima. Para cancelarlo hay que llamar a reset_estilo
 void imprimir_estilo(texto_atributo_t atributo, color_t color_texto, modo_texto_t modo_texto, color_t color_fondo, modo_fondo_t modo_fondo);
 
+// Setea el estilo a como viene por defecto en la terminal.
 void reset_estilo();
 
 #endif /* ESTILOS_H */
